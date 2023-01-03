ff6507a8448e61b90c296939f35a7a2f784070e5
mathcodes <jonpchristie@gmail.com>
2023-01-03 14:28:29 -0500

    pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import TourCard from './components/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Grid container spacing={5}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/components/TourCard.js
@@ -0,0 +1,81 @@
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import AccessTime from '@mui/icons-material/AccessTime';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: { 
+            variant: 'body2' 
+          },
+          style: {
+            fontSize: 11,
+            backgroundColor: 'red',
+          }
+        },
+        {
+          props: { 
+            variant: 'body3' 
+          },
+          style: {
+            fontSize: 9,
+          }
+        },
+      ]
+    }
+  }
+});
+
+
+const TourCard = () => {
+  return <Grid item xs={3}>
+    <ThemeProvider theme={theme}>
+    <Paper elevation={3}>
+      <img
+        src="https://cdn.pixabay.com/photo/2020/04/16/11/41/waterfall-5050298__480.jpg"
+        alt=""
+        className="img" />
+      <Box paddingX={1}>
+        <Typography variant="subtitle1" component="h2">
+          Niagara Falls
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <AccessTime sx={{ width: 12.5 }} />
+          <Typography variant="body2" component="p" marginLeft={0.5}>
+            5 Hours
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+          marginTop={3 }
+        >
+        <Rating 
+          name="read-only" 
+          value={4.5}
+          precision={0.5}
+          size="small"
+          readOnly 
+        />
+
+        </Box>
+      </Box> 
+    </Paper>
+    </ThemeProvider>
+  </Grid>
+}
+
+export default TourCard;
